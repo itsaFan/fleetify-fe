6c9fcd04e1a1cfa52097f4688209f1badcef925e
itsaFan <steffansim@gmail.com>
2025-09-16 20:29:00 +0700

    fix: remove axios cridentials

--- src/configs/api-config.ts
+++ src/configs/api-config.ts
@@ -4,7 +4,7 @@ import axios, { AxiosError } from "axios";
 export const api = axios.create({
   baseURL: process.env.NODE_ENV === "development" ? "v1" : "https://api-steffansim-fleetify.zeabur.app/v1",
   timeout: 10000,
-  withCredentials: true,
+  // withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
